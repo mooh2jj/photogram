aba7408415f9c8d5f5c24406d59251d02c30e532
mooh2jj <mooh2jj@naver.com>
2021-11-22 22:36:31 +0900

    [update] Validation 설정 && BindingResult 구현

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,9 +1,17 @@
 package com.cos.photogramstart.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.service.AuthService;
@@ -31,14 +39,28 @@ public class AuthController {
 	}
 	
 	@PostMapping("/signup")
-	public String signup(SignupDto signupDto) {
-		log.info("signup 실행됨");
-		log.info("signupDto: {}", signupDto.toString());
-		User user = signupDto.toEntity();
-		log.info("user: {}", user);
+	@ResponseBody
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
 		
-		User userEntity = authService.join(user);
-		log.info("userEntity: {}", userEntity);
+		if(bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for(FieldError error : bindingResult.getFieldErrors()) {		// @Valid에서 오류난 거
+				errorMap.put(error.getField(), error.getDefaultMessage());	// bindingResult에 모아짐!				
+				log.error("error.getDefaultMessage: {}", error.getDefaultMessage());
+			}
+			return "오류남";
+		} else {
+			log.info("signup 실행됨");
+			log.info("signupDto: {}", signupDto.toString());
+			User user = signupDto.toEntity();
+			log.info("user: {}", user);
+			
+			User userEntity = authService.join(user);
+			log.info("userEntity: {}", userEntity);
+		}
+		
+		
 		return "redirect:signin";
 	}
 }
--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
@@ -7,9 +11,14 @@ import lombok.Data;
 @Data
 public class SignupDto {
 
+	@Size(min = 4, max = 10)
+	@NotBlank
 	private String username;
+	@NotBlank
 	private String password;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String name;
 	
 	public User toEntity() {
