e5fa312b4303ff02a3c208a463c455d22fc12044
mooh2jj <mooh2jj@naver.com>
2021-11-24 14:08:09 +0900

    [add&update] ajax update 사용하기 update.js 구현

---
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
@@ -0,0 +1,31 @@
+package com.cos.photogramstart.web.dto.user;
+
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateDto {
+
+	private String name;	// 필수
+	private String password;	// 필수
+	private String website;
+	private String bio;
+	private String phone;
+	private String gender;
+	
+	// 조금 위험 : 코드 수정 불가피
+	public User toEntity() {
+		return User.builder()
+				.name(name)
+				.password(password)
+				.website(website)
+				.bio(bio)
+				.phone(phone)
+				.gender(gender)
+				.build();
+	}
+	
+	
+}
